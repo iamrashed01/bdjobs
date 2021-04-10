4a8479663e70c5b02f23a8a77426f76084873608
iamrashed01 <irashad42@gmail.com>
2021-04-10 08:39:11 +0600

    put extra line break before return

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,5 +5,6 @@ module.exports.getProfileController = async (req, res) => {
   const user = await User.findOne({ email: req.user.email }).select(
     '-password -email_verification_code',
   );
+
   return successSend(res, 200, user, 'User retrieved successfully');
 };
